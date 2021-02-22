913237c470df93e2f4755068c207245ebcb04c8b
Pedro Sanção <pedrosancao@users.noreply.github.com>
2021-02-22 19:27:08 -0300

    refactor: Simplify holiday service

--- services/holidays/index.js
+++ services/holidays/index.js
@@ -1,5 +1,5 @@
 /**
- * Tabela da lua cheia de Páscoa, valida entre 1900 e 2199 inclusive
+ * Tabela da lua cheia de Páscoa, valida entre 1900 e 2199, inclusive
  */
 function getPascalFullMoonDates() {
   return [
@@ -26,66 +26,119 @@ function getPascalFullMoonDates() {
 }
 
 /**
+ * Formata a data para meia noite, houveram alguns problemas com fuso
+ * horário e toLocaleString() causando a exibição da data anterior.
+ *
+ * @param Date date
+ * @return string
+ */
+function formatDateToMidnight(date) {
+  const formatedDay = `0${date.getDate()}`.substr(-2);
+  const formatedMonth = `0${date.getMonth() + 1}`.substr(-2);
+  return `${date.getFullYear()}-${formatedMonth}-${formatedDay} 00:00:00`;
+}
+
+/**
  * Cálculo de feriados móveis baseados na Páscoa
  *
  * @see https://en.wikipedia.org/wiki/Computus
  */
 function computus(year) {
+  if (year < 1900 || year > 2199) {
+    throw new Error('Cannot calculate holidays.');
+  }
   const pascalFullMoonMonthDay = getPascalFullMoonDates();
-  const goldenNumber = year % 19;
-  const [refMonth, refDay] = pascalFullMoonMonthDay[goldenNumber];
+  const [refMonth, refDay] = pascalFullMoonMonthDay[year % 19];
   const refDate = new Date(year, refMonth, refDay);
-  const holidays = Array(12)
-    .fill('')
-    .map(() => []);
+  const holidays = [];
   refDate.setDate(refDate.getDate() + 7 - refDate.getDay());
-  holidays[refDate.getMonth()][refDate.getDate()] = ['Páscoa'];
-  refDate.setDate(refDate.getDate() - 47);
-  holidays[refDate.getMonth()][refDate.getDate()] = ['Carnaval'];
+  holidays.push({
+    date: formatDateToMidnight(refDate),
+    name: 'Páscoa',
+  });
+  refDate.setDate(refDate.getDate() - 2);
+  // Sexta feira Santa / Paixão de Cristo não é feriado nacional
+  refDate.setDate(refDate.getDate() - 45);
+  holidays.push({
+    date: formatDateToMidnight(refDate),
+    name: 'Carnaval',
+  });
   refDate.setDate(refDate.getDate() + 107);
-  holidays[refDate.getMonth()][refDate.getDate()] = ['Corpus Christi'];
+  holidays.push({
+    date: formatDateToMidnight(refDate),
+    name: 'Corpus Christi',
+  });
   return holidays;
 }
 
 /**
  * Combina feriados móveis e fixos.
  *
+ * Lei n° 6.802 de 30/06/1980
+ * - Nossa Senhora Aparecida
+ * Lei n° 662 de 06/04/1949
+ * - Confraternização mundial
+ * - Tiradentes
+ * - Dia do trabalho
+ * - Independência do Brasil
+ * - Finados
+ * - Proclamação da República
+ * - Natal
+ *
  * Referência de https://github.com/pagarme/business-calendar/tree/master/src/brazil
  */
-function getNacionalHolidaysAsMatrix(year) {
-  if (year < 1900 || year > 2199) {
-    throw new Error('Cannot calculate holidays.');
-  }
+function getNacionalHolidaysUnordered(year) {
   const holidays = computus(year);
-  holidays[0][1] = ['Confraternização mundial'];
-  holidays[3][21] = [...(holidays[3][21] || []), 'Tiradentes'];
-  holidays[4][1] = ['Dia do trabalho'];
-  holidays[8][7] = ['Independência do Brasil'];
-  holidays[9][12] = ['Nossa Senhora Aparecida'];
-  holidays[10][2] = ['Finados'];
-  holidays[10][15] = ['Proclamação da República'];
-  holidays[11][25] = ['Natal'];
+  holidays.push(
+    {
+      date: `${year}-01-01 00:00:00`,
+      name: 'Confraternização mundial',
+    },
+    {
+      date: `${year}-04-21 00:00:00`,
+      name: 'Tiradentes',
+    },
+    {
+      date: `${year}-05-01 00:00:00`,
+      name: 'Dia do trabalho',
+    },
+    {
+      date: `${year}-09-07 00:00:00`,
+      name: 'Independência do Brasil',
+    },
+    {
+      date: `${year}-10-12 00:00:00`,
+      name: 'Nossa Senhora Aparecida',
+    },
+    {
+      date: `${year}-11-02 00:00:00`,
+      name: 'Finados',
+    },
+    {
+      date: `${year}-11-15 00:00:00`,
+      name: 'Proclamação da República',
+    },
+    {
+      date: `${year}-12-25 00:00:00`,
+      name: 'Natal',
+    }
+  );
   return holidays;
 }
 
-export default function getNacionalHolidays(year) {
-  const holidayMatrix = getNacionalHolidaysAsMatrix(year);
-  const holidays = holidayMatrix.map((monthMatrix, month) => {
-    const monthHolidays = monthMatrix.map((dayMatrix, day) => {
-      return dayMatrix.map((name) => {
-        const formatedDay = `0${day}`.substr(-2);
-        const formatedMonth = `0${month + 1}`.substr(-2);
-        return {
-          date: `${year}-${formatedMonth}-${formatedDay} 00:00:00`,
-          name,
-        };
-      });
-    });
-    return monthHolidays.reduce((accumulator, dayHolidays) => {
-      return accumulator.concat(dayHolidays);
-    }, []);
+function sortByDate(holidays) {
+  return holidays.sort((a, b) => {
+    if (a.date > b.date) {
+      return 1;
+    }
+    if (a.date < b.date) {
+      return -1;
+    }
+    return 0;
   });
-  return holidays.reduce((accumulator, monthHolidays) => {
-    return accumulator.concat(monthHolidays);
-  }, []);
+}
+
+export default function getNacionalHolidays(year) {
+  const holidays = getNacionalHolidaysUnordered(year);
+  return sortByDate(holidays);
 }
